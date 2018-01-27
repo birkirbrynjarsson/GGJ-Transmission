f340bfbe95db87ff633f97c94f693637ed499b9c
tinnathuridur <tinnats15@ru.is>
2018-01-27 17:09:20 +0000

    you can now quit in editor mode

--- Assets/Scripts/QuitButton.cs
+++ Assets/Scripts/QuitButton.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 public class QuitButton : MonoBehaviour {
 
     public void Quit() {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
         Application.Quit();
+        #endif
     }
 }
