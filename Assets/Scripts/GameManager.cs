c1a5df976ad0c0e8820975851c8d6a50fd0677a8
tinnathuridur <tinnats15@ru.is>
2018-01-27 18:55:47 +0000

    made a gamemanager that is a singleton and manages the players scores, the scores are updated on the UI (fully implemented in my sandbox scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    // using a singleton pattern for the gamemanager
+    public static GameManager gm = null;
+    // variables to keep track of the score for each player
+    // should be updated in one script and fetched to update UI in the UpdateScore script
+    public uint Player1Score;
+    public uint Player2Score;
+          
+    void Awake() {
+        if (gm == null) {
+            gm = this;
+            Player1Score = 0;
+            Player2Score = 0;
+        }
+
+        else if (gm != this) {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
